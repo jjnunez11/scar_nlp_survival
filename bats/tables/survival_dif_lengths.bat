f5ca9f6308762e3ee1646f2784a7445f0acb56dd
jjnunez <jjnunez11@gmail.com>
2022-10-22 13:24:21 -0700

    Removing images from viz folder

--- bats/tables/survival_dif_lengths.bat
+++ bats/tables/survival_dif_lengths.bat
@@ -8,10 +8,10 @@
 :: python -m models.cnn --target "surv_mo_36" --table "survival_dif_lengths" --table_extra "36"
 :: python -m models.cnn --target "surv_mo_60" --table "survival_dif_lengths" --table_extra "60"
 
-python -m models.lstm --target "surv_mo_6" --table "survival_dif_lengths" --table_extra "6"
-python -m models.lstm --target "surv_mo_36" --table "survival_dif_lengths" --table_extra "36"
-python -m models.lstm --target "surv_mo_60" --table "survival_dif_lengths" --table_extra "60"
+:: python -m models.lstm --target "surv_mo_6" --table "survival_dif_lengths" --table_extra "6"
+:: python -m models.lstm --target "surv_mo_36" --table "survival_dif_lengths" --table_extra "36"
+:: python -m models.lstm --target "surv_mo_60" --table "survival_dif_lengths" --table_extra "60"
 
-python -m models.bert --target "surv_mo_6" -batch-size 8 --table "survival_dif_lengths_extra" --table_extra "6"
-python -m models.bert --target "surv_mo_36" -batch-size 8 --table "survival_dif_lengths_extra" --table_extra "36"
-python -m models.bert --target "surv_mo_60" -batch-size 8 --table "survival_dif_lengths_extra" --table_extra "60"
+python -m models.bert --target "surv_mo_6" --batch-size 8 --table "survival_dif_lengths_extra" --table_extra "6"
+python -m models.bert --target "surv_mo_36" --batch-size 8 --table "survival_dif_lengths_extra" --table_extra "36"
+python -m models.bert --target "surv_mo_60" --batch-size 8 --table "survival_dif_lengths_extra" --table_extra "60"
